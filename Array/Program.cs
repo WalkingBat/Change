2eadd8fdf0dc8580910be0b3b5501e6154faa9c4
WalkingBat <90095809+WalkingBat@users.noreply.github.com>
2022-01-02 21:02:29 +0300

    Создание массива с рандомными числами

---
+++ Array/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] NewArray = new int[8];
+System.Console.WriteLine("Массив: ");
+for (int i = 0; i < NewArray.Length; i++)
+{
+    NewArray[i] = new Random().Next(1,10);
+    System.Console.Write($"{NewArray[i]}");
+}
+System.Console.WriteLine();
+
