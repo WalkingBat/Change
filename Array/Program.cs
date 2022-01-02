fe155c362292054bd2abb35b8ae1fb1edf19301a
WalkingBat <90095809+WalkingBat@users.noreply.github.com>
2022-01-02 21:09:20 +0300

    Массив с четными числами на основе предыдущего

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,4 +6,8 @@ for (int i = 0; i < NewArray.Length; i++)
     System.Console.Write($"{NewArray[i]}");
 }
 System.Console.WriteLine();
-
+System.Console.WriteLine("Массив с четными числами: ");
+for (int i = 0; i < NewArray.Length; i++)
+{
+    if (NewArray[i] % 2 == 0) System.Console.Write($"{NewArray[i]} ");
+}
